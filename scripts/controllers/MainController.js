5f7d231ae076f6d57c25d9d0d95a3c9ec74b395f
Luke <kevinkevin112@hotmail.com>
2017-02-18 19:41:30 +1100

    - Added: Bunch more data

--- scripts/controllers/MainController.js
+++ scripts/controllers/MainController.js
@@ -172,7 +172,7 @@ app.controller('MainController', ['$scope', function($scope) {
 
             var googleMap = new google.maps.Map(mapContainer, {
               center: {lat: -37.815112, lng: 144.960909},
-              zoom: 12,
+              zoom: 14,
               disableDefaultUI: true,
               styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"road.highway","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#bcbbbb"},{"visibility":"on"}]}]
             });
