2a78323e2740f765e56005ac43a0cbf0d6cffc9f
Luke <kevinkevin112@hotmail.com>
2017-03-09 20:36:55 +1100

    - Added: Directive templateUrls

--- scripts/directives/appfooter.js
+++ scripts/directives/appfooter.js
@@ -1,28 +1,6 @@
 birthdayBountyApp.directive('appfooter', function() {
   return {
     scope: false,
-    template: '<div class="footer-strip">' +
-                 '<div class="footer-text">' +
-                     '<a target="_blank" href="http://www.lukehedin.com/">' +
-                     	'Created by Luke Hedin' +
-                     '</a>' +
-                     '<div>' +
-                         'Bounty data collected with help from <a target="_blank" href="https://www.linkedin.com/in/andrew-gierens-638b727b">Andrew Gierens</a> and <a target="_blank" href="https://www.instagram.com/malitoburito/">Mali Lewis</a>' +
-                     '</div>' +
-                     '<div>' +
-                         'Social icons designed by <a target="_blank" href="http://www.flaticon.com/authors/freepik">Freepik</a> of Flaticon' +
-                     '</div>' +
-                     '<div>' +
-                         'Bounty icons designed by <a target="_blank" href="http://www.flaticon.com/authors/madebyoliver">Madebyoliver</a> of Flaticon' +
-                     '</div>' +
-                     '<br/>' +
-                     '<div>' +
-                         'Your birthday is {{ getBirthdayString() }}' +
-                     '</div>' +
-                     '<a href="" ng-click="clearBirthday()" ng-if="dob">' +
-                         'Click here to change your date of birth' +
-                     '</a>' +
-                 '</div>' +
-             '</div>'
+    templateUrl: './scripts/templates/appfooter.html'
   }
 });
--- scripts/directives/appheader.js
+++ scripts/directives/appheader.js
@@ -1,16 +1,6 @@
 birthdayBountyApp.directive('appheader', function() {
   return {
     scope: false,
-    template: '<div class="top-navigation-bar">' +
-                '<img class="flag-icon" src="images/flag.svg">' +
-                '<div class="nav-right">' +
-                    '<h4 class="tagline">{{ tagline }}</h4>' +
-                    '<a class="standard-button" ng-click="clearBirthday()" ng-if="dob">Change Birthday</a>' +
-                    '<a class="standard-button" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fbirthdaybounty.com.au%2F&amp;src=sdkpreparse">Share on Facebook</a>' +
-                '</div>' +
-                '<div class="logo-text">' +
-                    'BIRTHDAY<br/>BOUNTY' +
-                '</div>' +
-            '</div>'
+    templateUrl: './scripts/templates/appheader.html'
   }
 });
